67571128b3b2a7148c16f1b98933559bc5364b39
Efimenko Aleksandr <gazservissana.zp@gmail.com>
2018-04-24 15:07:05 +0300

    Initial Commit

---
+++ Logbook/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Logbook
+//
+//  Created by mac on 24.04.2018.
+//  Copyright © 2018 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Logbook/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Logbook
+//
+//  Created by mac on 24.04.2018.
+//  Copyright © 2018 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
